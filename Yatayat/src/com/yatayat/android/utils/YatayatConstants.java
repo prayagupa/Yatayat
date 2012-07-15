385e539ea39f00e2e13ebfd0da25925080e8d74d
iPrayag <prayag.upd@gmail.com>
2012-07-16 02:08:57 +0545

    [add YatayatConstants]

---
+++ Yatayat/src/com/yatayat/android/utils/YatayatConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.yatayat.android.utils;
+
+/**
+ * @author prayag
+ * @created 15 Jul 2012
+ * @filename YatayatConstants.java
+ */
+public class YatayatConstants {
+	public static final String STOPS_LIST_URL = "http://yyapi.monsooncollective.org/";
+}
