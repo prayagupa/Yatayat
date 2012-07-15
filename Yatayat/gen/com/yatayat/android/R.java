1475b1d043e29920d23f6d2e29fa54dddd3cde67
samrakchan <samrakchan@gmail.com>
2012-07-15 12:19:39 +0545

    First Screen layout
    Custom First Screen layout

--- Yatayat/gen/com/yatayat/android/R.java
+++ Yatayat/gen/com/yatayat/android/R.java
@@ -13,11 +13,18 @@ public final class R {
     public static final class color {
         public static final int app_color=0x7f040000;
         public static final int bg=0x7f040001;
-        public static final int darkBlue=0x7f040005;
-        /**  Action Bar 
+        public static final int black=0x7f040003;
+        public static final int button_border=0x7f040008;
+        public static final int button_border_selected=0x7f040009;
+        public static final int button_text=0x7f040007;
+        public static final int darkBgButton=0x7f04000c;
+        public static final int darkBlue=0x7f040006;
+        public static final int lightBgButton=0x7f04000a;
+        /**  Action Bar and Buttons 
          */
-        public static final int lightBlue=0x7f040003;
-        public static final int mediumBlue=0x7f040004;
+        public static final int lightBlue=0x7f040004;
+        public static final int mediumBgButton=0x7f04000b;
+        public static final int mediumBlue=0x7f040005;
         public static final int white=0x7f040002;
     }
     public static final class dimen {
@@ -27,15 +34,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int actionbar=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int logo_actionbar=0x7f020003;
+        public static final int button_normal=0x7f020001;
+        public static final int button_selected=0x7f020002;
+        public static final int button_selector=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int logo_actionbar=0x7f020006;
     }
     public static final class id {
         public static final int actionbar=0x7f090000;
-        public static final int actionbar_rl=0x7f090001;
-        public static final int actionbar_tv=0x7f090002;
-        public static final int menu_settings=0x7f090003;
+        public static final int actionbar_rl=0x7f090004;
+        public static final int actionbar_tv=0x7f090005;
+        public static final int end_point_et=0x7f090002;
+        public static final int find_vehicle_b=0x7f090003;
+        public static final int menu_settings=0x7f090006;
+        public static final int start_point_et=0x7f090001;
     }
     public static final class layout {
         public static final int activity_yatayat=0x7f030000;
@@ -46,12 +59,17 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f060000;
+        public static final int end_point=0x7f060005;
+        public static final int find_vehicle=0x7f060006;
         public static final int hello_world=0x7f060001;
         public static final int menu_settings=0x7f060002;
+        public static final int na=0x7f060007;
+        public static final int start_point=0x7f060004;
         public static final int title_activity_yatayat=0x7f060003;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
+        public static final int Button=0x7f070003;
         public static final int logo_text=0x7f070001;
         public static final int shadow_onthe_up_text=0x7f070002;
     }
