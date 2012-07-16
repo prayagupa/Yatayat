72c9ef19dd3c50b60c513a62b2e4097133a38323
iPrayag <prayag.upd@gmail.com>
2012-07-17 03:53:47 +0545

    [update yatayatutility.java]

--- Yatayat/src/com/yatayat/android/utils/YatayatUtility.java
+++ Yatayat/src/com/yatayat/android/utils/YatayatUtility.java
@@ -13,20 +13,24 @@ import android.net.NetworkInfo;
  * @filename YatayatUtility.java
  */
 public class YatayatUtility {
-	public boolean checkInternetConnection(Context context) {
+	public static boolean checkInternetConnection(Context context) {
 		boolean haveConnectedWifi = false;
 		boolean haveConnectedMobile = false;
 
 		ConnectivityManager cm = (ConnectivityManager) context
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-		for (NetworkInfo ni : netInfo) {
-			if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-				if (ni.isConnected())
-					haveConnectedWifi = true;
-			if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
-				if (ni.isConnected())
-					haveConnectedMobile = true;
+		try {
+			NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+			for (NetworkInfo ni : netInfo) {
+				if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+					if (ni.isConnected())
+						haveConnectedWifi = true;
+				if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+					if (ni.isConnected())
+						haveConnectedMobile = true;
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
 		}
 		return haveConnectedWifi || haveConnectedMobile;
 	}
