985216c3377a1798c37515dbf9673ff0e9e2e7a7
iPrayag <prayag.upd@gmail.com>
2012-07-21 16:44:49 +0545

    [add entity Vehicle.java]

---
+++ Yatayat/src/com/yatayat/android/models/Vehicle.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.yatayat.android.models;
+
+/**
+ * @author prayag
+ * @created 21 Jul 2012
+ * @filename Vehicle.java
+ */
+public class Vehicle {
+	private String ref;
+	private String transport;
+
+	public String getRef() {
+		return ref;
+	}
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+	public String getTransport() {
+		return transport;
+	}
+
+	public void setTransport(String transport) {
+		this.transport = transport;
+	}
+
+}
