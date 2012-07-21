268cdd686fde627ab801dd6f7ec1823d244e5a7a
iPrayag <prayag.upd@gmail.com>
2012-07-21 16:46:47 +0545

    [add Errorcode.java]

---
+++ Yatayat/src/com/yatayat/android/utils/ErrorCode.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.yatayat.android.utils;
+
+/**
+ * @author prayag
+ * @created 21 Jul 2012
+ * @filename ErrorCode.java
+ */
+public class ErrorCode {
+	public static final String NO_ROUTES_FOUND = "no routes found";
+}
