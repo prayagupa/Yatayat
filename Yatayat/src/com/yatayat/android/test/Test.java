3fa51b63919762be21cb28e1ce20b70b49b7d69c
iPrayag <prayag.upd@gmail.com>
2012-07-21 16:54:19 +0545

    [add Test]

---
+++ Yatayat/src/com/yatayat/android/test/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.yatayat.android.test;
+
+/**
+ * @author prayag
+ * @created 21 Jul 2012
+ * @filename Test.java
+ */
+public class Test {
+	public long startStopID = 2269119;
+	public long goalStopID = 1274184084;
+
+	public long getStartStopID() {
+		return startStopID;
+	}
+
+	public long getGoalStopID() {
+		return goalStopID;
+	}
+}
